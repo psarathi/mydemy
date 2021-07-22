382c8cf9c0fbd8dcb671cbaa22db71c1f9fd9eb7
Partha Sarathi <parthasarathi@gmail.com>
2021-07-22 01:22:02 -0700

    Adding revalidation and some formatting changes

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
-import { promises as fs } from "fs";
-import path from "path";
-import Landing from "../components/layout/Landing";
-import styles from "../styles/Home.module.css";
+import {promises as fs} from 'fs';
+import path from 'path';
+import Landing from '../components/layout/Landing';
+import styles from '../styles/Home.module.css';
 
-export default function Home({ files }) {
-  return (
-    <div className={styles.container}>
-      <Landing files={files} />
-    </div>
-  );
+export default function Home({files}) {
+    return (
+        <div className={styles.container}>
+            <Landing files={files} />
+        </div>
+    );
 }
 
 export async function getStaticProps() {
-  const currentDirectory = path.join(process.cwd(), "public");
-  console.log(currentDirectory);
-  const filenames = await fs.readdir(currentDirectory);
+    const currentDirectory = path.join(process.cwd(), 'public');
+    console.log(currentDirectory);
+    const filenames = await fs.readdir(currentDirectory);
 
-  const files = filenames.map((filename) => {
-    const filePath = path.join(currentDirectory, filename);
+    const files = filenames.map((filename) => {
+        const filePath = path.join(currentDirectory, filename);
 
+        return {
+            filePath,
+        };
+    });
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
     return {
-      filePath,
+        props: {
+            files,
+        },
+        revalidate: 10,
     };
-  });
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      files,
-    },
-  };
 }
