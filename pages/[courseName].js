7f256dad8d9e7bba3cbfc4ded77465df9312dc59
Partha Sarathi <parthasarathi@gmail.com>
2021-07-31 01:48:05 -0700

    added some layout using grid and styling

--- pages/[courseName].js
+++ pages/[courseName].js
@@ -6,28 +6,31 @@ import courses from '../courses.json';
 function CourseName({courseName}) {
     const course = courses.find((c) => c.name === courseName);
     return course ? (
-        <div>
-            <h3>{courseName}</h3>
-            <ul>
-                {course.topics.map((topic, i) => (
-                    <li key={i}>
-                        <strong>{topic.name}</strong>
-                        <ul>
-                            {topic.files
-                                .filter((f) => f.ext === '.mp4')
-                                .map((file, i) => (
-                                    <li key={i}>
-                                        <Link
-                                            href={`/${COURSE_PATH}/${course.name}/${topic.name}/${file.fileName}`}
-                                        >
-                                            <a>{file.name}</a>
-                                        </Link>
-                                    </li>
-                                ))}
-                        </ul>
-                    </li>
-                ))}
-            </ul>
+        <div className='courseWrapper'>
+            <div className='courseListings'>
+                <div className='courseName'>{courseName}</div>
+                <ul>
+                    {course.topics.map((topic, i) => (
+                        <li key={i} className='topicName'>
+                            <strong>{topic.name}</strong>
+                            <ul>
+                                {topic.files
+                                    .filter((f) => f.ext === '.mp4')
+                                    .map((file, i) => (
+                                        <li key={i} className='videoFile'>
+                                            <Link
+                                                href={`/${COURSE_PATH}/${course.name}/${topic.name}/${file.fileName}`}
+                                            >
+                                                <a>{file.name}</a>
+                                            </Link>
+                                        </li>
+                                    ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className='videoPlayer'>Video player coming soon...</div>
         </div>
     ) : (
         <div>{`${courseName} is not a valid course name`}</div>
