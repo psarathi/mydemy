b44769fe97cbc59a74d7b3e007fff06f60693de1
psarathi <parthasarathi@gmail.com>
2025-08-28 23:22:30 -0700

    Downgrade to Next.js 12.1.6 for ARM compatibility
    - Next.js 12 uses Babel by default, avoiding SWC binary issues
    - Remove all SWC workarounds and Babel dependencies
    - Simplify configuration back to basics
    - This should resolve ARM/Raspberry Pi build issues completely

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  // Reduce memory usage and worker processes for ARM/Raspberry Pi
-  experimental: {
-    workerThreads: false,
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Limit parallel processing for ARM devices
-    config.parallelism = 1;
-    return config;
-  }
 };
 
 module.exports = nextConfig;
