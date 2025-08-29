cbd080f40ab49bdd224a34a3bc042c8472b65d4e
psarathi <parthasarathi@gmail.com>
2025-08-28 23:11:46 -0700

    Force Babel compilation with webpack override for ARM build
    - Add webpack configuration to force babel-loader usage
    - Add babel-loader dependency
    - Completely bypass SWC compilation on ARM architecture

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Disable SWC for ARM architecture compatibility (Raspberry Pi)
+  // Completely disable SWC for ARM/Raspberry Pi
   swcMinify: false,
+  // Use Terser for minification instead of SWC
+  compiler: {},
   experimental: {
     forceSwcTransforms: false,
+  },
+  // Ensure webpack is used for compilation
+  webpack: (config, { dev, isServer }) => {
+    // Force use of Babel loader for JS/TS files
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel']
+        }
+      }
+    });
+    return config;
   }
 };
 
