7a98e3cbe9d38e63b17fa2ea8eb5087d2efc2a16
psarathi <parthasarathi@gmail.com>
2025-08-28 23:17:50 -0700

    Add NEXT_FORCE_BABEL environment variable for ARM builds
    - Simplify next.config.js to minimal configuration
    - Add NEXT_FORCE_BABEL=1 in .env.local to disable SWC
    - Add build:arm script for explicit ARM builds
    - This should resolve SWC binary loading issues on Raspberry Pi

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Completely disable SWC for ARM/Raspberry Pi
   swcMinify: false,
-  // Use Terser for minification instead of SWC
-  compiler: {},
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  // Ensure webpack is used for compilation
-  webpack: (config, { dev, isServer }) => {
-    // Force use of Babel loader for JS/TS files
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel']
-        }
-      }
-    });
-    return config;
-  }
 };
 
 module.exports = nextConfig;
