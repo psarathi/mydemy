74c992ad3ff4b6efe35189b9c7f189cc009c582b
psarathi <parthasarathi@gmail.com>
2025-08-28 23:28:24 -0700

    Upgrade to Next.js 15 with improved ARM support
    - Upgrade Next.js to latest version (15.x)
    - Update React to 18.3.x for compatibility
    - Restore useSearchParams from next/navigation
    - Latest Next.js has better ARM/SWC binary support

--- pages/[courseName].js
+++ pages/[courseName].js
@@ -1,14 +1,14 @@
 import Link from 'next/link';
-import {useRouter} from 'next/router';
+import {useSearchParams} from 'next/navigation';
 import React, {useEffect, useState, useRef} from 'react';
 import VideoPlayer from '../components/player/VideoPlayer';
 import {BASE_CDN_PATH, LOCAL_CDN} from '../constants';
 import courses from '../courses.json';
 
 function CourseName({courseName}) {
-    const router = useRouter();
-    const topic = router.query.topic;
-    const lesson = router.query.lesson;
+    const searchParams = useSearchParams();
+    const topic = searchParams.get('topic');
+    const lesson = searchParams.get('lesson');
     const course = courses.find((c) => c.name === courseName);
     const videoFileList = course
         ? course.topics.flatMap((t) => {
